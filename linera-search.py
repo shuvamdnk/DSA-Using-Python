4192935741091c55f8a582d7ba4e13a7c9fd2e7d
Shuvam <shubhamduttadnk@gmail.com>
2022-01-08 12:28:21 +0530

    first commit

---
+++ linera-search.py
@@ -0,0 +1,22 @@
+def linear_search(arr,key):
+    for i in range(len(arr)):
+        if arr[i] == key:
+            print(f'{key} Found at position {i}')
+            break
+    else:
+        print(f"{key} not found")    
+
+        
+
+
+
+n = int(input("Enter Array Size "))
+ele = []
+for i in range(n):
+    e = int(input())
+    ele.append(e)
+
+key = int(input('Enter the key search : '))
+linear_search(ele,key)
+
+
