332ba3fe84a8d8c23d3f3e452c0822a35b50ada1
shuvamdnk <80211038+shuvamdnk@users.noreply.github.com>
2026-06-01 20:15:05 +0530

    kadane algo update

--- Data Structures/Linear/Array/05_kadane_algorithm.py
+++ Data Structures/Linear/Array/05_kadane_algorithm.py
@@ -3,22 +3,19 @@ import math
 arr = [2, 3, -8, 7, -1, 2, 3]
 size = len(arr)
 
-# for i in range(size):
-#     for j in range(i+1):
-#         for k in range(j,i+1):
-#             print(arr[k], end="")
-#         print("", end=" ")
-#     print("")
+for start in range(size):
+    for end in range(start, size):
+        print(arr[start:end+1])
 
 # 53. Maximum Subarray - Kadane's Algorithm - LeetCode
 
-current_sum = 0
-max_sum = -math.inf
-for i in range(size-1):
-    current_sum += arr[i]
-    max_sum = max(current_sum, max_sum)
+# current_sum = 0
+# max_sum = -math.inf
+# for i in range(size-1):
+#     current_sum += arr[i]
+#     max_sum = max(current_sum, max_sum)
 
-    if current_sum < 0:
-        current_sum = 0
+#     if current_sum < 0:
+#         current_sum = 0
 
-print(max_sum)
+# print(max_sum)
